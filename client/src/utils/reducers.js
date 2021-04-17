af33dc09c92285d8569701ec12337364974049b0
Niles87 <nbing19@gmail.com>
2021-04-17 10:17:07 -0700

    updated reducers and removed GlobalState from redux store

--- client/src/utils/reducers.js
+++ client/src/utils/reducers.js
@@ -9,9 +9,16 @@ import {
   CLEAR_CART,
   TOGGLE_CART,
 } from './actions';
-import { useReducer } from 'react';
 
-export const reducer = (state, action) => {
+const initialState = {
+  products: [],
+  categories: [],
+  currentCategory: '',
+  cart: [],
+  cartOpen: false,
+};
+
+export const reducer = (state = initialState, action) => {
   switch (action.type) {
     case UPDATE_PRODUCTS:
       return {
@@ -76,6 +83,4 @@ export const reducer = (state, action) => {
   }
 };
 
-export function useProductReducer(initialState) {
-  return useReducer(reducer, initialState);
-}
+export default reducer;
